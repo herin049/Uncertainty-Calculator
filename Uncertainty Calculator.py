9805fe0a5cdb891f5790c59008c9e273d0014f76
herin049 <40302054+herin049@users.noreply.github.com>
2018-11-02 13:40:32 -0500

    Update Uncertainty Calculator.py

--- Uncertainty Calculator.py
+++ Uncertainty Calculator.py
@@ -12,28 +12,28 @@ class var:
         
     def __add__(self, other):
         l = pow(self.getdelta(), 2) + pow(other.getdelta(), 2)
-        return variable(self.getvalue() + other.getvalue(), pow(l, 0.5))
+        return var(self.getvalue() + other.getvalue(), pow(l, 0.5))
 
     def __sub__(self, other):
         l = pow(self.getdelta(), 2) + pow(other.getdelta(), 2)
-        return variable(self.getvalue() -  other.getvalue(), pow(l, 0.5))
+        return var(self.getvalue() -  other.getvalue(), pow(l, 0.5))
         
     def __mul__(self, other):
         l = ((pow(self.getdelta() * other.getvalue(), 2)) + (pow(other.getdelta() * self.getvalue(), 2)))
-        return variable(self.getvalue() * other.getvalue(), pow(l, 0.5))
+        return var(self.getvalue() * other.getvalue(), pow(l, 0.5))
     
     def __div__(self, other):
         a = -1 * pow(other.getvalue(),-2) * self.getvalue()
         b = 1 / other.getvalue()
         l = (pow(self.getdelta() * b, 2) + pow(other.getdelta() * a, 2))
-        return variable(self.getvalue() / other.getvalue(), pow(l, 0.5))
+        return var(self.getvalue() / other.getvalue(), pow(l, 0.5))
         
     def __pow__(self, const):
         l = const * pow(self.getvalue(), const - 1) * self.getdelta()
-        return variable(pow(self.getvalue(), const), l)
+        return var(pow(self.getvalue(), const), l)
     
     def __str__(self):
-        return str(k.getvalue()) + " , " + str(k.getdelta()) 
+        return str(k.getvalue()) + " ± " + str(k.getdelta()) 
 
         
 
@@ -41,9 +41,9 @@ class var:
 m = var(3, 0.1)
 v = var(4, 0.2)
 
-# k = 1/2 mv^2
 k = var(0.5) * m * (v ** 2)
+
 
 print(k)
 
-# Output : 24.0 , 2.529822128134704
+# Output: 24.0 ± 2.529822128134704
